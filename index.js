d26b557d10cba2fc663ae160a341635c6f9941bd
Ganisher <ganisherbaxtiyarov0732@gmail.com>
2025-11-13 16:04:01 +0500

    .

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+import TelegramBot from "node-telegram-bot-api";
+import fs from "fs";
+
+const Token = "8574611961:AAEBL94vubBaqQXr56gc6h-NJLF7JzkbNKY";
+const bot = new TelegramBot(Token, { polling: true });
+
+
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  const firstname = msg.chat.first_name;
+
+   if (text === "/start") {
+    await bot.sendMessage(chatId,`Assalomu alaykum, ${firstname}! Botga xush kelibsiz!` , {
+        reply_markup: {
+            keyboard: [
+                [{text: "100x Academy Fanlari 👨‍💻"}],
+                [{text: "Ro'yhatdan o'tish"}],
+            ],
+        },
+    });
+  }
+});
+
+
 console.log("Bot ishga tushdi");
